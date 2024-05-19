323a53c0a68cec6fdeaa34c25345c4c9b357ff7d
Gousepeer Arella <111117491+gousepeer3919@users.noreply.github.com>
2024-05-19 19:16:26 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5001/", {
+			const socket = io("https://interestbuddy.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
